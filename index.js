2bb11e8a6233ca6702d9175fd6221c647822dae8
Tempus <tempusware@gmail.com>
2019-06-30 19:47:07 +1000

    Version 1.2 - Fixed stuff and added stuff
    Fixed:
    - Number guessing game taking a guess immediately after the game starts
    - Cleaned up Unscramble game subcommands system
    - Added hint subcommand to Unscramble game
    - Added caption command based off my caption-adder tool
    - Fixed Unscramble game not accepting words with a hyphen

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const Canvas = require("canvas");
+const { Image } = require("canvas");
+//const fetch = require('node-fetch');
+const fetch = require("node-superfetch");
 
 client.once("ready", () => {
 	console.log("Ready!");
@@ -7,18 +11,19 @@ client.once("ready", () => {
 });
 
 client.login(process.env.BOT_TOKEN);
+//console.log("Remember to remove the bot token when commiting to GitHub!");
 
 const helpEmbed = new Discord.RichEmbed()
 	.setColor("#78f7fe")
 	.setTitle("List of Commands")
-	.setDescription("Commands in UPPERCASE are main commands. Subcommands are in lowercase and are optional unless marked with *. \nExample: -mathsquestion-multiplication-12")
+	.setDescription("Commands in UPPERCASE are main commands. Subcommands are in lowercase and are optional unless written in italics. \nExamples: -maths-multiplication-12 | -caption-My name is-JEFF-#-https://website.com/image.png")
 
 	.addField("-UNSCRAMBLE", "Get a word to unscramble.")
 
 	.addField("-NUMBERGUESS", "Guess a random generated number.")
 	.addField("-range", "Choose the range for the random number to generate up to. Default range is 10.", true)
 
-	.addField("-8BALL-*question", "Ask the Magic 8-Ball a question and get a response.")
+	.addField("-8BALL-*question*", "Ask the Magic 8-Ball a question and get a response.")
 
 	.addField("-MATHS / -MATHSQUESTION", "Get a maths question to answer.")
 	.addField("-add / -addition", "Get an addition question.", true)
@@ -33,9 +38,11 @@ const helpEmbed = new Discord.RichEmbed()
 	.addField("-LOWERCASE", "Convert a message to all lower case.", true)
 	.addField("-UPPERCASE", "Convert a message to all upper case.", true)
 
+	.addField("-CAPTION-*toptext*-*bottomtext*-*smalltext*-image.png", "Add a caption to an image. Use # if you don't want to use a text. If an image is attached to the message, image.png isn't necessary. Use \\n to insert line breaks. Use \\aL at the start of an input to align text to the left or \\aR for the right. Browser version: https://tempusware.github.io/caption-adder/")
+
 	.setTimestamp();
 
-client.on("message", message => {
+client.on("message", async message => {
   if (message.author.bot) {return};
 	var messageContent = message.content;
 	var args = message.content.split("-");
@@ -53,30 +60,47 @@ client.on("message", message => {
 				message.channel.send(helpEmbed);
 				break;
 			case "unscramble":
-				if (args[1] && args[1].toLowerCase() === "end") {
-					message.channel.send("Unscramble game ended. The word was **'" + unscrambledWord + "'**.");
-					unscrambledWord = "";
+				// Subcommands
+				if (args[1]) {
+					switch (args[1]) {
+						case "end":
+							message.channel.send("Unscramble game ended. The word was **'" + unscrambledWord + "'**.");
+							unscrambledWord = "";
+							break;
+						case "hint":
+							message.channel.send("Rescrambled: " + unscrambledWord.shuffle());
+							break;
+						default:
+							message.reply("That's not a valid subcommand!");
+					};
 					return;
 				};
+				// New game
 				unscrambledWord = words[Math.round(Math.random() * words.length)];
 				message.channel.send("Unscramble this: " + unscrambledWord.shuffle());
 				break;
 			case "numberguess":
-				if (args[1] && isNaN(args[1]) && args[1].toLowerCase() === "end") {
-					message.channel.send("Number guessing game ended. The number was **'" + thoughtNumber + "'**.");
-					thoughtNumber = false;
-					return;
-				} else if (args[1] && isNaN(args[1])) {
-					message.reply("That's not a valid subcommand.");
-					return;
-				};
-				if (args[1] && !isNaN(args[1])) {
-					var number = args[1];
+				if (args[1]) {
+					if (isNaN(args[1])) {
+						switch (args[1].toLowerCase()) {
+							case "end":
+								message.channel.send("Number guessing game ended. The number was **'" + thoughtNumber + "'**.");
+								thoughtNumber = false;
+								return;
+								break;
+							default:
+								message.reply("That's not a valid subcommand.");
+								return;
+						};
+					} else {
+						var number = args[1];
+					};
 				} else {
 					var number = 10;
 				};
 				thoughtNumber = Math.floor((Math.random() * number) + 1);
 				message.channel.send("What number from 1-" + number + " am I thinking of?");
+				return;
 				break;
 			case "8ball":
 				if (!args[1]) {
@@ -141,6 +165,7 @@ client.on("message", message => {
 						mathsAnswer = "nogame";
 						break;
 					default:
+						message.reply("That's not a valid subcommand!");
 				};
 				break;
 			case "illiterate": case "ilr":
@@ -161,15 +186,141 @@ client.on("message", message => {
 			case "uppercase":
 				message.channel.send(messageContent.toUpperCase());
 				break;
+			case "caption":
+				if (!args[3]) {
+					message.reply("Not enough arguments. Command usage is as follows: -caption-Top text-Bottom text-Small text-https://website.com/image.png. To leave a text blank, use # in its place. An image url must be provided if an image isn't attached. PNGs only.");
+					return;
+				};
+
+				// Get the image
+				var imgSrc;
+				if (message.attachments.array()[0]) {
+					imgSrc = message.attachments.array()[0].url;
+					console.log("IMAGE ATTACHED.")
+				};
+				if (args[4] && args[4].endsWith(".png")) {
+					imgSrc = args[4];
+					console.log("IMAGE LINKED.")
+				};
+				if (!imgSrc) {
+					console.log("NO IMAGE LOADED.");
+					message.reply("No image was attached or linked. Only PNGs are accepted. If you're linking an image (using a url), this command can't accept filenames with a hyphen (-).");
+					return;
+				};
+
+				// Get the text
+				var toptext = args[1];
+				if (toptext != "#") {
+					toptext = toptext.split("\\n");
+				} else {
+					toptext = [];
+				};
+				var bottomtext = args[2];
+				if (bottomtext != "#") {
+					bottomtext = bottomtext.split("\\n");
+				} else {
+					bottomtext = [];
+				};
+				var smalltext = args[3];
+
+				// Image loader taken from https://www.youtube.com/watch?v=A1dSjB8z0JI
+				const { body: imgFetch } = await fetch.get(imgSrc);
+				const img = await Canvas.loadImage(imgFetch);
+
+				console.log(toptext + bottomtext + smalltext + imgSrc);
+				console.log("Loading image: " + imgSrc);
+				message.reply("Loading image...");
+
+				// Create the canvas
+				var captionHeight = 90;
+				var outputWidth = 1280;
+				var resizedHeight = outputWidth / img.width * img.height;
+				var canvas = Canvas.createCanvas(outputWidth, resizedHeight + captionHeight * toptext.length);
+				var context = canvas.getContext("2d");
+				context.lineWidth = 2;
+				context.font = "50px arial";
+				context.textAlign = "center";
+				var textstart = outputWidth / 2;
+
+				// Add top text
+				if (toptext.length != 0) {
+					context.fillStyle = "white";
+					context.fillRect(0, 0, outputWidth, captionHeight * toptext.length);
+					context.fillStyle = "black";
+					if (toptext[0].startsWith("\\aL")) {
+						toptext[0] = toptext[0].substr(3);
+						context.textAlign = "left";
+						textstart = 25
+					} else if (toptext[0].startsWith("\\aR")) {
+						toptext[0] = toptext[0].substr(3);
+						context.textAlign = "right";
+						textstart = outputWidth - 25
+					}
+					for (var i = 0; i < toptext.length; i++) {
+						context.fillText(toptext[i], textstart, captionHeight / 3 * 2 + captionHeight * i);
+					};
+					context.textAlign = "center";
+					textstart = outputWidth / 2;
+				}
+
+				context.drawImage(img, 0, 0, img.width, img.height, 0, captionHeight * toptext.length, outputWidth, resizedHeight);
+
+				if (bottomtext.length != 0) {
+					context.fillStyle = "rgba(0, 0, 0, 0.7)";
+					context.fillRect(0, canvas.height - captionHeight * bottomtext.length, outputWidth, canvas.height);
+					context.fillStyle = "white";
+					if (bottomtext[0].startsWith("\\aL")) {
+						bottomtext[0] = bottomtext[0].substr(3);
+						context.textAlign = "left";
+						textstart = 25
+					} else if (bottomtext[0].startsWith("\\aR")) {
+						bottomtext[0] = bottomtext[0].substr(3);
+						context.textAlign = "right";
+						textstart = outputWidth - 25
+					};
+					for (var i = 0; i < bottomtext.length; i++) {
+						context.fillText(bottomtext[i], textstart, canvas.height - captionHeight / 3 - captionHeight * i)
+					}
+					context.textAlign = "center";
+					textstart = outputWidth / 2;
+				};
+
+				if (smalltext != "#") {
+					context.fillStyle = "white";
+					context.font = "30px arial";
+					context.textAlign = "right";
+					if (smalltext.startsWith("\\aL")) {
+						smalltext = smalltext.substr(3);
+						context.textAlign = "left";
+						textstart = 10
+					} else if (smalltext.startsWith("\\aR")) {
+						smalltext = smalltext.substr(3);
+						context.textAlign = "right";
+						textstart = canvas.width - 10
+					} else if (smalltext.startsWith("\\aC")) {
+						smalltext = smalltext.substr(3)
+					} else {
+						context.textAlign = "right";
+						textstart = canvas.width - 10
+					}
+					context.fillText(smalltext, textstart, canvas.height - 10 - captionHeight * bottomtext.length)
+				};
+
+				const attachment = new Discord.Attachment(canvas.toBuffer(), "image.png");
+				message.reply("Here is your image:", attachment);
+
+				break;
 			default:
 				break;
 		};
 	};
 
 	// Responses
-	if (unscrambledWord && messageContent.toLowerCase().includes(unscrambledWord)) {
-		message.reply("You got the word! The word was **'" + unscrambledWord + "'**.");
-		unscrambledWord = "";
+	if (unscrambledWord) {
+		if (messageContent.toLowerCase().includes(unscrambledWord) || message.content.toLowerCase().includes(unscrambledWord)) {
+			message.reply("You got the word! The word was **'" + unscrambledWord + "'**.");
+			unscrambledWord = "";
+		};
 	};
 	if (thoughtNumber && !isNaN(messageContent)) {
 		if (messageContent == thoughtNumber) {
@@ -236,7 +387,7 @@ var words = [
 	"gauntlet",
 ];
 var unscrambledWord;
-var thoughtNumber;
+var thoughtNumber = false;
 var responsesBoolean = [
 	"Definitely not. (Captain Marvel 77:18)",
 	"Definitely not. (Doctor Strange 11:22)",
